ac22d23b26ccde11bcd4be727cb362235c9f0cc6
Thomas Stensitzki <thomas@mcsmemail.de>
2022-09-05 15:07:03 +0200

    Update Script
    Typos fixed + PS hygiene

--- Export-MessageQueue.ps1
+++ Export-MessageQueue.ps1
@@ -1,34 +1,35 @@
 <#
     .SYNOPSIS
-    Export messages from a transport queue to file system for manual replay 
-   
-    Thomas Stensitzki
-	
-    THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
-    RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
-	
-    Version 1.1, 2017-01-05
+    Export messages from a transport queue to file system for manual replay
 
-    Ideas, comments and suggestions to support@granikos.eu 
- 
-    .LINK  
-    More information can be found at http://scripts.granikos.eu
-	
+    Thomas Stensitzki
+
+    THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE
+    RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+
+    Version 1.2, 2022-09-05
+
+    Leave ideas, comments, and suggestions at GitHub
+
+    .LINK
+    https://github.com/Apoc70/Export-MessageQueue
+
     .DESCRIPTION
-	
+
     This script suspends a transport queue, exports the messages to the local file system. After successful export the messages are optionally deleted from the queue.
-    
-    .NOTES 
-    Requirements 
-    - Exchange Server 2013+
-    - Windows Server 2012 R2  
+
+    .NOTES
+    Requirements
+    - Exchange Server 2016+
+    - Windows Server 2016+
     - Utilizes global functions library
 
-    Revision History 
-    -------------------------------------------------------------------------------- 
-    1.0     Initial community release 
+    Revision History
+    --------------------------------------------------------------------------------
+    1.0     Initial community release
     1.1     Some PowerShell hygiene
-	
+    1.2     PowerSHell Typo fixed and updated for Exchange Server 2019
+
     .PARAMETER Queue
     Full name of the transport queue, e.g. SERVER\354
     Use Get-Queue -Server SERVERNAME to identify message queue
@@ -71,31 +72,33 @@ else {
   Write-Warning -Message 'Please check http://bit.ly/GlobalFunctions for further instructions'
   exit
 }
-$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+
+$ScriptDir = Split-Path -Path $script:MyInvocation.MyCommand.Path
 $ScriptName = $MyInvocation.MyCommand.Name
 $logger = New-Logger -ScriptRoot $ScriptDir -ScriptName $ScriptName -LogFileRetention 14
 $logger.Write('Script started')
-$logger.Write("Working on message queue $($Queue), Export folder: $($Path), DeleteAfterExport: $($DeleteAfterExport)")
+$logger.Write(('Working on message queue {0}, Export folder: {1}, DeleteAfterExport: {2}' -f $Queue, $Path, $DeleteAfterExport))
 
 ### FUNCTIONS -----------------------------
 
 function Request-Choice {
+    [CmdletBinding()]
     param([string]$Caption)
     $choices =  [System.Management.Automation.Host.ChoiceDescription[]]@("&Yes","&No")
     [int]$defaultChoice = 1
 
     $choiceReturn = $Host.UI.PromptForChoice($Caption, "", $choices, $defaultChoice)
 
-    return $choiceReturn   
+    return $choiceReturn
 }
 
 function Check-Folders {
     # Check, if export folder exists
-    if(!(Test-Path $Path)) {
+    if(!(Test-Path -Path $Path)) {
         # Folder does not exist, lets create a new root folder
         New-Item -Path $Path -ItemType Directory | Out-Null
-        
-        $logger.Write("Folder $($Path) created")
+
+        $logger.Write(('Folder {0} created' -f $Path))
     }
 }
 
@@ -106,11 +109,11 @@ function Check-Queue {
     try {
         $messageQueue = Get-Queue $Queue
         $messageCount = $messageQueue.MessageCount
-        
-        $logger.Write("$($messageCount) message(s) found in queue $($Queue)")
+
+        $logger.Write(('{0} message(s) found in queue {1}' -f $messageCount, $Queue))
     }
     catch {
-        $logger.Write("Queue $($Queue) cannot be accessed")
+        $logger.Write(('Queue {0} cannot be accessed' -f $Queue))
     }
     $messageCount
 }
@@ -119,28 +122,43 @@ function Export-Messages {
     # Export suspended messages
     try {
         # Suspend messages in queue
-        $logger.Write("Suspending queue $($Queue)")
+        $logger.Write(('Suspending queue {0}' -f $Queue))
         Get-Queue $Queue | Get-Message -ResultSize Unlimited | Suspend-Message -Confirm:$false
 
         # Fetch suspended messages
-        $logger.Write("Fetching suspended messages from queue $($Queue)")
-        
-        $messages = @(Get-Message $Queue -ResultSite Unlimited | Where-Object{$_.Status -eq "Suspened"} )
+        $logger.Write(('Fetching suspended messages from queue {0}' -f $Queue))
 
-        $logger.Write("$($messages.Count) suspended messages fetched from queue $($Queue)")
+        $messages = @(Get-Queue $Queue | Get-Message -ResultSize Unlimited | Where-Object{$_.Status -eq "Suspended"} )
+
+        $logger.Write( ('{0} suspended messages fetched from queue {1}' -f $messages.Count, $Queue) )
 
         # Export fetched messages
-        $messages | ForEach-Object {$m++;Export-Message $_.Identity | AssembleMessage -Path (Join-Path "$($m).eml" -Path $Path)}
+        $messages | ForEach-Object {$m++;Export-Message $_.Identity | AssembleMessage -Path (Join-Path -ChildPath ('{0}.eml' -f $m) -Path $Path)}
     }
-    catch {}
+    catch {
+      # get error record
+      [Management.Automation.ErrorRecord]$e = $_
+
+      # retrieve information about runtime error
+      $info = [PSCustomObject]@{
+        Exception = $e.Exception.Message
+        Reason    = $e.CategoryInfo.Reason
+        Target    = $e.CategoryInfo.TargetName
+        Script    = $e.InvocationInfo.ScriptName
+        Line      = $e.InvocationInfo.ScriptLineNumber
+        Column    = $e.InvocationInfo.OffsetInLine
+      }
+      
+      Write-Error $info
+    }
 }
 
 function Delete-Messages {
     # Delete suspended messages from queue
-    
-    $logger.Write("Delete  suspended messages from queue $($Queue)")
-    
-    Get-Message -Queue $Queue -ResultSize Unlimited | Where-Object{$_.Status -eq "Suspened"} | Remove-Message -WithNDR $false -Confirm:$false 
+
+    $logger.Write( ('Delete  suspended messages from queue {0}' -f $Queue) )
+
+    Get-Message -Queue $Queue -ResultSize Unlimited | Where-Object{$_.Status -eq "Suspened"} | Remove-Message -WithNDR $false -Confirm:$false
 }
 
 
@@ -151,7 +169,7 @@ Check-Folders
 
 # 2. Check queue
 if((Check-Queue -gt 0)) {
-    if((Request-Choice -Caption "Do you want to suspend and export all messages in queue $($Queue)?") -eq 0) {
+    if((Request-Choice -Caption ('Do you want to suspend and export all messages in queue {0}?' -f $Queue)) -eq 0) {
         # Yes, we want to suspend and delete messages
         Export-Messages
     }
@@ -160,11 +178,11 @@ if((Check-Queue -gt 0)) {
         $logger.Write("User choice: Do not suspend and export messages")
     }
     if($DeleteAfterExport) {
-        if((Request-Choice -Caption "Do you want to DELETE all suspended messages from queue $($Queue)?") -eq 0) {
+        if((Request-Choice -Caption ('Do you want to DELETE all suspended messages from queue {0}?' -f $Queue)) -eq 0) {
             $logger.Write("User choice: DELETE suspended")
             Write-Output "Suspended messages will be deleted WITHOUT sending a NDR!"
-            Delete-Messages 
-        } 
+            Delete-Messages
+        }
         else {
             $logger.Write("User choice: DO NOT DELETE suspended")
             Write-Output "Exported messages have NOT been deleted from queue!"
@@ -173,8 +191,8 @@ if((Check-Queue -gt 0)) {
     }
 }
 else {
-    Write-Output "Queue $($Queue) does not contain any messages"
-    $logger.Write("Queue $($Queue) does not contain any messages")
+    Write-Output ('Queue {0} does not contain any messages' -f $Queue)
+    $logger.Write(('Queue {0} does not contain any messages' -f $Queue))
 }
 
 $logger.Write("Script finished")
